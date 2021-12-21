f24393734cebf608ecbe9e9f6cfddc5137f9419b
weslley-sc <weslleydasilvacostaa@gmail.com>
2021-12-21 17:09:34 -0300

    dizendo obrigado ao novo pull request

--- src/action.js
+++ src/action.js
@@ -1,5 +1,19 @@
+const core = require('@actions/core'); 
+const github = require('@actions/github'); 
+
 async function run() {
-    console.log('Hello, World!');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+        const octokit = github.getOctokit(GITHUB_TOKEN);
+
+        const { context = {} } = github;
+        const { pull_request } = context.payload;
+
+        await octokit.issues.createComment({
+            ...context.repo,
+            issue_number: pull_request.number,
+            body: 'Thank you for submitting a pull request! we will try to review this as soon we can.'
+        })
 }
 
 run();
